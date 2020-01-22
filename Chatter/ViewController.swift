df14e0958a1249d7f5bde620bc2deec90a127060
nikhilbh <nikhilbh@umich.edu>
2020-01-22 11:42:34 -0500

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Nikhil Bhansali on 1/22/20.
+//  Copyright © 2020 nikhilbh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
